236ff90850cdaa5acda5038df3c15bc35e82edac
paparves11@gmail.com <paparves11@gmail.com>
2018-09-08 23:09:59 -0400

    editedv2

--- src/arraylist/example/UseOfIterator.java
+++ src/arraylist/example/UseOfIterator.java
@@ -11,13 +11,16 @@ public class UseOfIterator {
         list.add(60);
         list.add(80);
         list.add(90);
+        list.add(50);
 
         Iterator it = list.iterator();
         while(it.hasNext()){
             System.out.println(it.next());
         }
         System.out.println("Java Project 8 correction 1 shared with Shazzad Mahmud");
-        System.out.println("Edited for Ali");
+        System.out.println("Edited for Ali Done by hasan");
+
+        System.out.println("Edit by parves");
     }
 
 }
